b978ba681b571d86dba5ac6e6dc814b433d643af
aaakash06 <akashbagale182@gmail.com>
2024-04-10 23:38:48 +0545

    filter type error fixed

--- src/components/shared/Filter/Filter.tsx
+++ src/components/shared/Filter/Filter.tsx
@@ -9,82 +9,58 @@ import {
 } from "@/components/ui/select";
 import Tag from "../TagComponent/Tag";
 
-const FilterDropDown = ({ items, tags }: { items: string[], tags?: boolean}) => {
-
-if(tags){
-  return (
-  
-  
-    <>
-    <div className="md:hidden text-dark-300 poppins max-sm:text-sm dark:text-light-900 flex items-center  ">
-  
-  <Select >
-    <SelectTrigger className="w-[180px] background-light800_dark300 h-full border-hidden rounded-lg max-sm:min-w-full max-sm:p-4  ">
-      <SelectValue className=" " placeholder="Select a Filter " />
-    </SelectTrigger>
-    <SelectContent className=" background-light800_dark300 dark:text-light-900">
-      <SelectGroup>
-        {items.map((item) => (
-          <SelectItem key={item} value={item}>
-            {" "}
-            {item}{" "}
-          </SelectItem>
-        ))}
-      </SelectGroup>
-    </SelectContent>
-  </Select>
-  </div>
-  
-  <div className="flex gap-7 mt-7 max-md:hidden ">
-  {items.map((item) => (
-          <Tag  key={item} item={item} rounded="md" otherStyle="p-2 text-[12px]"/>
-        
-        ))}
-  
-  
-  </div>
-  
-  </>
-  
+const FilterDropDown = ({
+  items,
+  tags,
+}: {
+  items: string[];
+  tags?: boolean;
+}) => {
+  if (tags) {
+    return (
+      <>
+        <div className="md:hidden text-dark-300 poppins max-sm:text-sm dark:text-light-900 flex items-center  ">
+          <Select>
+            <SelectTrigger className="w-[180px] background-light800_dark300 h-full border-hidden rounded-lg max-sm:min-w-full max-sm:p-4  ">
+              <SelectValue className=" " placeholder="Select a Filter " />
+            </SelectTrigger>
+            <SelectContent className=" background-light800_dark300 dark:text-light-900">
+              <SelectGroup>
+                {items.map((item) => (
+                  <SelectItem key={item} value={item}>
+                    {" "}
+                    {item}{" "}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </>
     );
-
-
-}
-else{
-
-  return (
-  
-  
-    <>
-    <div className=" text-dark-300 poppins dark:text-light-900 flex items-center  ">
-  
-  <Select >
-    <SelectTrigger className="w-[180px] background-light800_dark300 h-full border-hidden rounded-lg max-sm:min-w-full max-sm:p-4 ">
-      <SelectValue className=" " placeholder="Select a Filter " />
-    </SelectTrigger>
-    <SelectContent className=" background-light800_dark300 dark:text-light-900">
-      <SelectGroup>
-        {items.map((item) => (
-          <SelectItem key={item} value={item}>
-            {" "}
-            {item}{" "}
-          </SelectItem>
-        ))}
-      </SelectGroup>
-    </SelectContent>
-  </Select>
-  </div>
-  
- 
-  
-  </>
-  
+  } else {
+    return (
+      <>
+        <div className=" text-dark-300 poppins dark:text-light-900 flex items-center  ">
+          <Select>
+            <SelectTrigger className="w-[180px] background-light800_dark300 h-full border-hidden rounded-lg max-sm:min-w-full max-sm:p-4 ">
+              <SelectValue className=" " placeholder="Select a Filter " />
+            </SelectTrigger>
+            <SelectContent className=" background-light800_dark300 dark:text-light-900">
+              <SelectGroup>
+                {items.map((item) => (
+                  <SelectItem key={item} value={item}>
+                    {" "}
+                    {item}{" "}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </>
     );
-
-
-}
-
-
+  }
 };
 
 export default FilterDropDown;
